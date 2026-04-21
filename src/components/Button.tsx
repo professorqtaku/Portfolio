6aafb28fd37450e81d849449c91bc9a7161a197c
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-21 14:19:31 +0200

    button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,19 @@
 interface WrapperProps {
   children: React.ReactNode;
-  onClick: () => void;
+  onClick?: () => void;
   buttonType?: 'primary' | 'secondary' | 'danger';
   className?: string;
 }
 const Button =({ children, onClick, buttonType = 'primary', className = "" }: WrapperProps) => {
 
+    const basicStyles = 'btn font-bold py-2 px-4 rounded hover:cursor-pointer hover:shadow-lg hover:-translate-y-1 transition duration-300 ease-in-out';
     const buttonStyles = {
-        primary: 'bg-primary text-text hover:bg-blue-700 text-white font-bold py-2 px-4 rounded hover: cursor-pointer hover: shadow-lg transition duration-300 ease-in-out   ',
-        secondary: 'bg-neutral-dark text-blue-text hover:opacity-90 font-bold py-2 px-4 rounded',
-        danger: 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded',
+        primary: 'bg-primary text-text',
+        secondary: 'bg-neutral-dark text-blue-text',
+        danger: 'bg-red-500 text-white',
     };
     return ( 
-        <button onClick={onClick} className={`btn hover:cursor-pointer hover: ${buttonStyles[buttonType]} ${className}`} type="button">
+        <button onClick={onClick} className={`${basicStyles} ${buttonStyles[buttonType]} ${className}`} type="button">
             {children}
         </button>
      );
