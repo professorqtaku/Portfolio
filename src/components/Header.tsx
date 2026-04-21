0849d6a23e9b8be8aa902756fb6262e32307c969
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-21 14:20:00 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,9 @@ import { NavLink } from "react-router";
 
 function Header() {
     return ( 
-        <header className="header">
-            <nav>
+        <header className="header bg-neutral text-text px-10 py-6 flex justify-between items-center">
+            <h1 className="text-2xl font-bold">Qiling Tan</h1>
+            <nav className="flex gap-4 text-lg">
                 <NavLink to="/" end>Home</NavLink>
                 <NavLink to="/experience">Experience</NavLink>
             </nav>
